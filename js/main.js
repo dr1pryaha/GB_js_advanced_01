ab3db0507a7c8ce4dd6a0106fa8cfa5084abb08a
Anton Pryakhin <dr.pryah@gmail.com>
2022-08-14 16:24:31 +0300

    lesson-1: add default values for function arguments

--- js/main.js
+++ js/main.js
@@ -3,17 +3,21 @@ const products = [
   { id: 2, title: "Mouse", price: 20, link: "/img/mouse.jpg" },
   { id: 3, title: "Keyboard", price: 200, link: "/img/keyboard.jpg" },
   { id: 4, title: "Gamepad", price: 50, link: "/img/gamepad.jpg" },
-  { id: 4, title: "Headphones", price: 500, link: "/img/headphones.jpg" },
-  { id: 4, title: "Monitor", price: 1500, link: "/img/monitor.jpg" },
-  { id: 4, title: "Monitor", price: 1500, link: "/img/monitor.jpg" },
-  { id: 4, title: "Monitor", price: 1500, link: "/img/monitor.jpg" },
-  { id: 4, title: "Monitor", price: 1500, link: "/img/monitor.jpg" },
-  { id: 4, title: "Monitor", price: 1500, link: "/img/monitor.jpg" },
+  { id: 5, title: "Headphones", price: 500, link: "/img/headphones.jpg" },
+  { id: 6, title: "Monitor", price: 1500, link: "/img/monitor.jpg" },
+  { id: 7 },
+  { id: 8 },
+  { id: 9 },
+  { id: 10 },
 ];
 
 //Функция для формирования верстки каждого товара
 //Добавить в выводе изображение
-const renderProduct = (title, price, link) => {
+const renderProduct = (
+  title = "Notebook",
+  price = 2000,
+  link = "/img/notebook.jpg"
+) => {
   return `<div class="products-item">
                   <img class="products-image" src="${link}" alt="product">
                   <h3 class="products-title">${title}</h3>
